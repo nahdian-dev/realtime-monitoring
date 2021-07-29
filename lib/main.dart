4b4586a0f5b4b1f83abf9225e077675190f8d2aa
Nahdian <nahdian7@gmail.com>
2021-07-29 18:10:27 +0700

    entry queue on drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:realtime_monitoring/views/entry_queue/entry_queue.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,10 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: EntryQueue(),
     );
   }
 }
